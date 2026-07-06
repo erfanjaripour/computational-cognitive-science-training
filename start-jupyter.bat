701d5be62028044209a9d281c9789dd3991108cd
Erfan Jaripour <myjaripour@gmail.com>
2026-07-06 13:44:47 +0430

    Add Jupyter startup batch script

---
+++ start-jupyter.bat
@@ -0,0 +1,7 @@
+@echo off
+
+call "%USERPROFILE%\miniforge3\Scripts\activate.bat" science
+
+cd /d "F:\doc-hub\education\psychology\projects\computational-cognitive-science-training"
+
+python -m notebook
