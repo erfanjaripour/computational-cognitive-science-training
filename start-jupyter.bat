d71509991df6500c44f4f9f2b368ea4dc03cee90
Erfan Jaripour <myjaripour@gmail.com>
2026-08-16 19:44:08 +0430

    Update gitignore and add Jupyter startup

--- start-jupyter.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call "%USERPROFILE%\miniforge3\Scripts\activate.bat" cog-modeling
-
-cd /d "F:\doc-hub\education\psychology\projects\computational-cognitive-science-training"
-
-python -m notebook
